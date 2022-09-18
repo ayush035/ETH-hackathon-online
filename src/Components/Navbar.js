baa8ec15d2e969105732bdade967c93424c90eae
Ayush Gupta <75483021+ayush035@users.noreply.github.com>
2022-09-18 21:05:31 +0530

    UP

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -13,7 +13,7 @@ export default function Navbar (){
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="#"><font color ='white' size="4">Home</font></a>
+          <a className="nav-link active" aria-current="page" href="/Upload"><font color ='white' size="4">Upload</font></a>
         </li>
         <li className="nav-item">
           <a className="nav-link" href="#"><font color ='white' size="4">My Gallery</font></a>
---
+++ src/Pages/Upload.js
@@ -0,0 +1,13 @@
+import { useMoralis } from "react-moralis"
+import React, { useEffect, useState } from "react"
+import Navbar from "../Components/Navbar"
+import Back from '../Components/Back'
+function Upload(){
+    return (
+        <><Navbar />
+        <Back /></>)
+
+}
+export default Upload;
+
+
